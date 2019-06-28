afb1b0fc31b8553469ab6acc9e6ec006fc13f642
pengboguo <45628160+pengboguo@users.noreply.github.com>
2019-06-28 14:53:31 +0800

    Update UserDao.java

--- src/main/java/com/offcn/dao/UserDao.java
+++ src/main/java/com/offcn/dao/UserDao.java
@@ -4,7 +4,7 @@ import com.offcn.po.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface UserDao extends JpaRepository<User,Long> {
-
+//***************************************************
     //xinzengfangfa1
     public User findByNameContaining(String keyword);
 
