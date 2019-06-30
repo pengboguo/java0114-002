33ad90f5d964b7aadad3c1ba52f24601bb7fa570
pengboguo <1586618537@qq.com>
2019-06-30 10:58:04 +0800

    开发者2新增方法

--- src/main/java/com/offcn/dao/UserDao.java
+++ src/main/java/com/offcn/dao/UserDao.java
@@ -8,4 +8,6 @@ public interface UserDao extends JpaRepository<User,Long> {
     //xinzengfangfa1
     public User findByNameContaining(String keyword);
 
+    //开发者2 新增方法
+    public void demo3(String name);
 }
